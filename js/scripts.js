b328c8c97177787723d7dc760c984a969ed51d13
Dexter Rualo <dexterrualo@gmail.com>
2017-01-24 20:56:54 +0800

    Added switch statement to handle key presses

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
   $(".button").click(function(){
+    switch (this.text()) {
+      case "1":
+      case "2":
+      case "3":
+      case "4":
+      case "5":
+      case "6":
+      case "7":
+      case "8":
+      case "9":
+      case "0":
+      case "+":
+      case "-":
+      case "=":
+      case "u00D7":
+      case "u00F7":
+    }
     $(".screen").text($(".screen").text() + $(this).text());
   });
 });
